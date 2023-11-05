1ff1b277f3a620b6fbce63783dab4db9f4480e18
DESKTOP-2IGR2C3\Korobela <gwala.lungeloe@gmail.com>
2023-11-05 20:31:29 +0200

    Add base project with auth

---
+++ src/KGB-System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KGB_System.Models
+{
+  public class ErrorViewModel
+  {
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
